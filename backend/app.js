8a7af85cbe04f520bda9d0f7bac78c831091dd01
Toshi <jpcmp3@gmail.com>
2025-06-02 23:48:30 +0300

    created model for Product, created Routes, added Controller, connected Database

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,7 @@
 import express from "express";
+import product from "./routes/productRoutes.js";
 const app = express();
+
+// Route
+app.use("/api/v1", product);
 export default app;
---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getAllProducts } from "../controller/productController.js";
+const router = express.Router();
+
+// Routes
+router.route("/products").get(getAllProducts);
+
+export default router;
