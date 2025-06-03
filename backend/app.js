8b1b1b5a036dd102c977136443abeb7570019ed5
Toshi <jpcmp3@gmail.com>
2025-06-03 20:16:57 +0300

    created post route for creating products, refined product model

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import product from "./routes/productRoutes.js";
 const app = express();
 
+// Middleware
+app.use(express.json());
+
 // Route
 app.use("/api/v1", product);
 export default app;
